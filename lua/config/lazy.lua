938cbe83c4cd6f671ef02a52f17ff8924de1ef54
phamnam2003 <namphamhai7@gmail.com>
2024-11-20 08:57:15 +0700

    fix(ui-editor): fix cannot load header and missing color

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.ui.dashboard-nvim" },
 
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
