7c39d74ca6477ce05af7518f6e18b30aad310297
phamnam2003 <namphamhai7@gmail.com>
2024-12-25 14:54:09 +0700

    feat(scroll): disabled animation smooth when scroll

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -60,15 +60,13 @@ return {
     },
   },
 
-  -- animations
+  -- disabled animations when scroll
   {
-    "echasnovski/mini.animate",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.scroll = {
-        enable = false,
-      }
-    end,
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+    keys = {},
   },
 
   -- buffer line
