bdf3d5279d6a582f2a8fd10e02786716addb8d40
phamnam2003 <namphamhai7@gmail.com>
2025-09-18 07:57:15 +0700

    refactor(func): change deprecated vim lua func

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,13 +46,6 @@ if vim.fn.has("nvim-0.8") == 1 then
   vim.opt.cmdheight = 0
 end
 
--- File types
-vim.filetype.add({
-  extension = {
-    mdx = "mdx",
-  },
-})
-
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_cmp = "blink.cmp"
