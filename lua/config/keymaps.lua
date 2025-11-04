eba4f072f450bcb6de9b530d629795999feda7d4
Pham Hai Nam <namphamhai7@gmail.com>
2025-11-04 16:00:20 +0700

    fix(hsl): replace HSL require with phamnam module

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -64,7 +64,7 @@ keymap.set("n", "<C-j>", function()
 end, opts)
 
 keymap.set("n", "<leader>r", function()
-  require("craftzdog.hsl").replaceHexWithHSL()
+  require("phamnam.hsl").replaceHexWithHSL()
 end)
 
 keymap.set("n", "<leader>i", function()
